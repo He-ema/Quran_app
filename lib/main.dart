5eeaa1cdc190bcb61fd787feef88acc1f6e71a34
ibrahim Osama <zx.hema72@gmail.com>
2023-08-13 07:18:17 +0300

    awesome notificationin drbug is displayed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -11,6 +13,24 @@ import 'package:prayer/pages/splash_screen.dart';
 import 'cubits/theme_cubit/theme_cubit.dart';
 import 'firebase_options.dart';
 
+Future<void> backgroundHandler(RemoteMessage message) async {
+  String? title = message.notification!.title;
+  String? body = message.notification!.body;
+  AwesomeNotifications().createNotification(
+      content: NotificationContent(
+    id: 123,
+    channelKey: 'firebase_channel',
+    color: Colors.green,
+    title: title,
+    body: body,
+    category: NotificationCategory.Message,
+    wakeUpScreen: true,
+    fullScreenIntent: true,
+    autoDismissible: true,
+    backgroundColor: Colors.orange,
+  ));
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -22,6 +42,22 @@ void main() async {
 
   await Hive.initFlutter();
   await Hive.openBox('quran');
+
+  AwesomeNotifications().initialize("resource://drawable/notification_icon", [
+    NotificationChannel(
+      channelKey: 'firebase_channel',
+      channelName: 'firebase channel',
+      channelDescription: 'firebase notification channel',
+      defaultColor: Colors.redAccent,
+      ledColor: Colors.white,
+      importance: NotificationImportance.Max,
+      channelShowBadge: true,
+      locked: true,
+      defaultRingtoneType: DefaultRingtoneType.Notification,
+    )
+  ]);
+  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
+
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
